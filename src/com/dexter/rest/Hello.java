ce1ea21695bdd44697a77cee8ba9309b6aecf567
dexterwei <dexterwei@163.com>
2015-06-10 14:43:48 -0700

    post request supported

--- src/com/dexter/rest/Hello.java
+++ src/com/dexter/rest/Hello.java
@@ -1,23 +1,45 @@
 package com.dexter.rest;
 
+import java.util.ArrayList;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/")
 public class Hello {
 	private static final String version="1.0.0";
-	
+	private static ArrayList<String> messages = new ArrayList<String>();
+	@Path("/hello/{visitor}")
 	@GET
 	@Produces(MediaType.TEXT_HTML)
-	public String ReturnTitleHTML(){
-		return "<h1>Hello Dexter</h1>";
+	public String ReturnTitleHTML(@PathParam("visitor") String Name){
+		messages.add(Name);
+		String rst="";
+		for(String msg :messages){
+			rst+="<p>"+msg+"</p>";
+		}
+		return "<h1>Hello "+Name + ", Welcome to Dexter's webapp</h1>"+rst;
 	}
 	
-
+	@Path("/hello/{visitor}")
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String ReturnTitleTEXT(){
-		return "Hello Dexter";
+	public String ReturnTitleTEXT(@PathParam("visitor") String Name){
+		messages.add(Name);
+		String rst="";
+		for(String msg :messages){
+			rst+=msg+"\n";
+		}
+		return "Hello "+Name + ", Welcome to Dexter's webapp\n"+rst;
+	}
+	
+	@Path("/messages")
+	@POST
+	@Consumes("text/plain")
+	public String postClichedMessage(String msg) {
+	    // Store the message
+		messages.add(msg);
+		return "copy that";
 	}
 	
 	@Path("/version")
