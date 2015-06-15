e063700ca9b806de8e3ef7340455d75df77d4af1
dexterwei <dexterwei@163.com>
2015-06-15 14:27:56 -0700

    add json support

---
+++ src/com/dexter/conversation.java
@@ -0,0 +1,7 @@
+package com.dexter;
+
+public class conversation {
+	public String sender;
+	public String receiver;
+	public String message;
+}
--- src/com/dexter/rest/Hello.java
+++ src/com/dexter/rest/Hello.java
@@ -4,11 +4,40 @@ import java.util.ArrayList;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.dexter.conversation;
 
 @Path("/")
 public class Hello {
+
 	private static final String version="1.0.0";
 	private static ArrayList<String> messages = new ArrayList<String>();
+	
+	private static ArrayList<conversation> chats = new ArrayList<conversation>();
+	
+	@Path("/chats/get")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<conversation> ReturnChatsJSON(){
+//		conversation chat = new conversation();
+//		chat.sender="Server";
+//		chat.receiver=Name;
+//		chat.message="Hi "+chat.receiver+", welcome to visit "+chat.sender;
+//
+//		chats.add(chat);
+		return chats;
+	}
+	
+	@Path("/chats/post")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response CreateChatsJSON(conversation chat){
+		String rcv=""+chat;
+		chats.add(chat);
+		return Response.status(201).entity(rcv).build();
+	}
+	
 	@Path("/hello/{visitor}")
 	@GET
 	@Produces(MediaType.TEXT_HTML)
