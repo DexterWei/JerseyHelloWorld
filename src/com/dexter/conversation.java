809e6e94ce65f9fb119ee3b7bab30f91615fd49f
dexterwei <dexterwei@163.com>
2015-06-16 01:19:47 -0700

    HW1

--- src/com/dexter/conversation.java
+++ src/com/dexter/conversation.java
@@ -4,4 +4,9 @@ public class conversation {
 	public String sender;
 	public String receiver;
 	public String message;
+	public String toString(){
+		String str="";
+		str="from:"+sender+",to:"+receiver+",message:"+message+"\n";
+		return str;
+	}
 }
--- src/com/dexter/rest/Hello.java
+++ src/com/dexter/rest/Hello.java
@@ -14,18 +14,19 @@ public class Hello {
 	private static final String version="1.0.0";
 	private static ArrayList<String> messages = new ArrayList<String>();
 	
-	private static ArrayList<conversation> chats = new ArrayList<conversation>();
+	private static ArrayList<ArrayList<conversation>> chats = new ArrayList<ArrayList<conversation>>();
+	public Hello(){
+		if(chats.isEmpty()){
+			for(int i=0;i<3;i++){
+				chats.add( new ArrayList<conversation>());
+			}
+		}
+	}
 	
 	@Path("/chats/get")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public ArrayList<conversation> ReturnChatsJSON(){
-//		conversation chat = new conversation();
-//		chat.sender="Server";
-//		chat.receiver=Name;
-//		chat.message="Hi "+chat.receiver+", welcome to visit "+chat.sender;
-//
-//		chats.add(chat);
+	public ArrayList<ArrayList<conversation>> ReturnChatsJSON(){
 		return chats;
 	}
 	
@@ -33,55 +34,9 @@ public class Hello {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response CreateChatsJSON(conversation chat){
-		String rcv=""+chat;
-		chats.add(chat);
-		return Response.status(201).entity(rcv).build();
+		chats.get(chat.sender.charAt(0)-'1').add(chat);
+		String rst=""+chats.get(chat.sender.charAt(0)-'1');
+		return Response.status(201).entity(rst).build();
 	}
 	
-	@Path("/hello/{visitor}")
-	@GET
-	@Produces(MediaType.TEXT_HTML)
-	public String ReturnTitleHTML(@PathParam("visitor") String Name){
-		messages.add(Name);
-		String rst="";
-		for(String msg :messages){
-			rst+="<p>"+msg+"</p>";
-		}
-		return "<h1>Hello "+Name + ", Welcome to Dexter's webapp</h1>"+rst;
-	}
-	
-	@Path("/hello/{visitor}")
-	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String ReturnTitleTEXT(@PathParam("visitor") String Name){
-		messages.add(Name);
-		String rst="";
-		for(String msg :messages){
-			rst+=msg+"\n";
-		}
-		return "Hello "+Name + ", Welcome to Dexter's webapp\n"+rst;
-	}
-	
-	@Path("/messages")
-	@POST
-	@Consumes("text/plain")
-	public String postClichedMessage(String msg) {
-	    // Store the message
-		messages.add(msg);
-		return "copy that";
-	}
-	
-	@Path("/version")
-	@GET
-	@Produces(MediaType.TEXT_HTML)
-	public String ReturnVersionHTML(){
-		return "<h3>version: "+ version+"</h3>";
-	}
-	
-	@Path("/version")
-	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String ReturnVersionTEXT(){
-		return "Version: "+version;
-	}
 }
